c26c754ebb727d34ac0032b8a8a19dc4c3fefc88
Prakash Prabhu <pprakashs@gmail.com>
2025-01-21 18:45:32 -0600

    code

--- python/basics/sliding_window/longest_substring.py
+++ python/basics/sliding_window/longest_substring.py
@@ -1,3 +1,7 @@
+
+#O(n)
+#O(n)
+
 def longest_substring(input : str) -> int:
     l=0
     r=0
@@ -29,6 +33,31 @@ def longest_substring(input : str) -> int:
                 l += 1
     return maxLength
 
+
+def longest_substring_2(input : str) -> int:
+    n = len(input)
+    l = 0
+    r = 0
+    words_set = set()
+    max_word_length = 0
+    for r in range(n):
+        if input[r] not in words_set:
+            words_set.add(input[r])
+            word_length = r - l + 1
+            max_word_length = max(max_word_length, word_length)
+        else:
+            print("words_set: " + str(words_set))
+            while(input[r] in words_set):
+                words_set.remove(input[l])
+                l += 1
+            words_set.add(input[r])
+        
+    return max_word_length
+
+
 input = "abcabcbb"
-length = longest_substring(input)
-print("longest substring: " + str(length))
+#length = longest_substring(input)
+#print("longest substring: " + str(length))
+
+length = longest_substring_2(input)
+print("longest substring 2: " + str(length))
