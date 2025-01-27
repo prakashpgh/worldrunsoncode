5365cdd0e3a594bdbd831dccf3eb0c1a147cd322
prakashpgh <pprakashs@gmail.com>
2025-01-26 21:30:29 -0600

    mono

--- python/basics/stack/monotonous_stack/sliding_window_max.py
+++ python/basics/stack/monotonous_stack/sliding_window_max.py
@@ -1,19 +1,26 @@
 '''
 https://www.geeksforgeeks.org/sliding-window-maximum-maximum-of-all-subarrays-of-size-k/
 
-https://www.youtube.com/watch?v=oaN9ibZKMpA
-
-
-this leverages the histogram problem... which can be solved by using stacks..
-    finding the minimum pre and post.
-    then multiply the pre and post
-
-    do this row by row pass the row to the histogram function
-    .... for each row, calculate the area.
-                        and find the max area
-        for each row if there is 1, add it to the previous row... 
-                    if there is zero, make the row element = 0
-
-                        https://www.youtube.com/watch?v=oaN9ibZKMpA
                         
 '''
+def max_subarray(input: list[int], k:int):
+    n = len(input)
+    result = [0] * (n-k+1)
+    
+    for i in range(n-k+1):
+        l = i 
+        r = i + k -1
+        max_item = 0
+        for j in range(l,r):
+            max_item = max(max_item, input[j])
+        result[i] = max_item    
+    return result
+
+
+
+
+input = [1, 2, 3, 1, 4, 5]
+k=3
+result = max_subarray(input, k)
+print("result: " + str(result))
+
